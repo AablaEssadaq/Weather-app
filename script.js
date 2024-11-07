d1c351b6c02a55949f3e9feee522905b4a43f025
Aabla Essadaq <149837893+AablaEssadaq@users.noreply.github.com>
2024-11-07 13:41:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let api_url = "http://api.weatherapi.com/v1/current.json?key=5aee125a73944ded995195639240511&q=";
+
+async function fetchData(){
+    try {
+        console.log(document.getElementById('city').value);
+        const response = await fetch(api_url+document.getElementById('city').value.toLowerCase());
+        const data = await response.json();
+        document.getElementById('cityName').textContent = data.location.name;
+        document.getElementById('degrees').textContent = data.current.temp_c + '°C';
+        document.getElementById('humidity').textContent = "Humidity: " + data.current.humidity + '%';
+        document.getElementById('weatherState').textContent = data.current.condition.text;
+        const iconUrl = `https:${data.current.condition.icon}`;
+        console.log(iconUrl);
+        document.getElementById('weather-icon').setAttribute("src", iconUrl);
+        
+    } catch (error) {
+        console.error("Oops something went wrong.")
+        alert("Location not found!");
+    }
+}
